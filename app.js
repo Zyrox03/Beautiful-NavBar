1c95f004be137f757c5e1111bcfc8a0487b9b473
Zyrox <anisanissou2003@gmail.com>
2022-09-21 09:54:42 +0100

    i made it responsive and added some java

---
+++ app.js
@@ -0,0 +1,13 @@
+const menuToggle = document.querySelector('.menuToggle');
+const navToggle = document.querySelector('.navBarToggle')
+
+
+    menuToggle.addEventListener('click',function(){
+        navToggle.classList.toggle('hide')
+       
+        
+    })
+
+
+
+    
